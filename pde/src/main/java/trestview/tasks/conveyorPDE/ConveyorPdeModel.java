703cd3cdf384de54f72c21bf0d0d4371a177bbd6
Max Pihnastyi <pmo707@bk.ru>
2016-07-24 00:40:38 +0300

    interface

--- pde/src/main/java/trestview/tasks/conveyorPDE/ConveyorPdeModel.java
+++ pde/src/main/java/trestview/tasks/conveyorPDE/ConveyorPdeModel.java
@@ -1,16 +1,36 @@
 package trestview.tasks.conveyorPDE;
 
-/**
- * Created by Max on 20.07.2016.
- */
-public class ConveyorPdeModel {
+import entityProduction.Machine;
 
+interface  IConveyorPdeModel {
+
+    public double initialCondition(double R);
+    public double boundaryCondition(double R);
+    public boolean comparisonBoundaryCondition(double R);
+    public boolean compari();
+    public double decision(double I,double B);
+    public double h(double R);
+
+}
+public class    ConveyorPdeModel {
+
+
+    public boolean isDddd() {
+        return dddd;
+    }
+
+    public boolean dddd;
 
     public ConveyorPdeModel() {
         System.out.println("Hello !!!!!!");
     }
 
+
+
+
+
     static public void main (String [] args) {
         ConveyorPdeModel conveyorPdeModel = new ConveyorPdeModel();
+
     }
 }
