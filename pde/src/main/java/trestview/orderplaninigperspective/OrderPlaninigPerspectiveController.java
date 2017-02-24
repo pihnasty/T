1c4d593af2c28b683aca92a0bbbe94e89947f986
Anton Iehorov <Iehorov@epam.com>
2017-02-24 22:36:07 +0200

    Added comment

--- pde/src/main/java/trestview/orderplaninigperspective/OrderPlaninigPerspectiveController.java
+++ pde/src/main/java/trestview/orderplaninigperspective/OrderPlaninigPerspectiveController.java
@@ -10,4 +10,5 @@ public class OrderPlaninigPerspectiveController extends InitializableDS {
     public OrderPlaninigPerspectiveController(ObservableDS observableDS) {
         super(observableDS);
     }
+//
 }
